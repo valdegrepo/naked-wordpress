6da62c6829d5edb9c668ecb625f1c4c84c82cb53
valdeg <mailme@valdeg.xyz>
2019-10-09 10:04:51 +0300

    Cleaned footer

--- footer.php
+++ footer.php
@@ -1,28 +1,8 @@
-<?php
-	/*-----------------------------------------------------------------------------------*/
-	/* This template will be called by all other template files to finish 
-	/* rendering the page and display the footer area/content
-	/*-----------------------------------------------------------------------------------*/
-?>
+</main>
 
-</main><!-- / end page container, begun in the header -->
+    <footer class="site-footer"></footer> 
 
-<footer class="site-footer">
-	<div class="site-info container">
-		
-		<p>Birthed <a href="http://bckmn.com/naked-wordpress" rel="theme">Naked</a> 
-			on <a href="http://wordpress.org" rel="generator">Wordpress</a> 
-			by <a href="http://bckmn.com" rel="designer">Joshua Beckman</a>
-		</p>
-		
-	</div><!-- .site-info -->
-</footer><!-- #colophon .site-footer -->
-
-<?php wp_footer(); 
-// This fxn allows plugins to insert themselves/scripts/css/files (right here) into the footer of your website. 
-// Removing this fxn call will disable all kinds of plugins. 
-// Move it if you like, but keep it around.
-?>
+    <?php wp_footer(); ?>
 
 </body>
 </html>
